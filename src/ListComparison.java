ddd7700e6464804ced1047e84e45b8ed8332341b
TatsianaMasiukevich <42011535+Tatsianacs@users.noreply.github.com>
2019-05-28 12:25:10 +0300

    refactor: optimized functions

--- src/ListComparison.java
+++ src/ListComparison.java
@@ -4,18 +4,21 @@ public class ListComparison {
 
     public static ArrayList<Integer> createArrayList() {
         ArrayList<Integer> collection = new ArrayList<>();
-        for (int i = 0; i < 10000000; i++) {
-            collection.add(generatedRandomInt());
-        }
+        createRequiredList(collection);
         return collection;
     }
 
     public static LinkedList<Integer> createLinkedList() {
         LinkedList<Integer> collection = new LinkedList<>();
-        for (int i = 0; i < 10000000; i++) {
-            collection.add(generatedRandomInt());
-        }
+        createRequiredList(collection);
         return collection;
+    }
+
+    private static List<Integer> createRequiredList(List<Integer> list) {
+        for (int i = 0; i < 10000000; i++) {
+            list.add(generatedRandomInt());
+        }
+        return list;
     }
 
     private static int generatedRandomInt() {
@@ -23,78 +26,57 @@ public class ListComparison {
     }
 
     public static long getTimeToAddToArrayList(ArrayList<Integer> arrList) {
-
-        long startTime = System.nanoTime();
-        for (int i = 0; i < 1000; i++) {
-            arrList.add(generatedRandomInt());
-        }
-        long endTime = System.nanoTime();
-
-        return endTime - startTime;
+        return getTimeToAddToList(arrList);
     }
 
-    public static long getTimeToAddTLinkedList(LinkedList<Integer> linkedList) {
-
-        long startTime = System.nanoTime();
-        for (int i = 0; i < 1000; i++) {
-            linkedList.add(generatedRandomInt());
-        }
-        long endTime = System.nanoTime();
-
-        return endTime - startTime;
+    public static long getTimeToAddToLinkedList(LinkedList<Integer> linkedList) {
+        return getTimeToAddToList(linkedList);
     }
 
+    private static long getTimeToAddToList(List<Integer> list) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            list.add(generatedRandomInt());
+        }
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
 
     public static long getTimeToRemoveFromArrayList(ArrayList<Integer> arrList) {
-        Iterator itr = arrList.iterator();
-        int i = 1000;
-        long startTime = System.nanoTime();
-        while (itr.hasNext() && i > 0) {
-            Integer val = (Integer)itr.next();
-            itr.remove();
-            i--;
-        }
-        long endTime = System.nanoTime();
-        return endTime - startTime;
-
+        return getTimeToRemoveFromList(arrList);
     }
 
     public static long getTimeToRemoveFromLinkedList(LinkedList<Integer> linkedList) {
-        Iterator itr = linkedList.iterator();
+        return getTimeToRemoveFromList(linkedList);
+    }
+
+    private static long getTimeToRemoveFromList(List<Integer> list) {
+        Iterator itr = list.iterator();
         int i = 1000;
         long startTime = System.nanoTime();
         while (itr.hasNext() && i > 0) {
-            Integer val = (Integer)itr.next();
+            Integer val = (Integer) itr.next();
             itr.remove();
             i--;
         }
         long endTime = System.nanoTime();
         return endTime - startTime;
-
     }
 
     public static long getTimeToSearchFromArrayList(ArrayList<Integer> arrList) {
-        int i = 1000;
-        long startTime = System.nanoTime();
-        while (i > 0) {
-            int searchValue = generatedRandomInt();
-            for (int val : arrList) {
-                if (val == searchValue) {
-                    break;
-                }
-            }
-            i--;
-        }
-        long endTime = System.nanoTime();
-        return endTime - startTime;
+        return getTimeToSearchFromList(arrList);
     }
 
     public static long getTimeToSearchFromLinkedList(LinkedList<Integer> linkedList) {
+        return getTimeToSearchFromList(linkedList);
+    }
+
+    private static long getTimeToSearchFromList(List<Integer> list) {
         int i = 1000;
         long startTime = System.nanoTime();
         while (i > 0) {
             int searchValue = generatedRandomInt();
-            for (int val : linkedList) {
+            for (int val : list) {
                 if (val == searchValue) {
                     break;
                 }
